611681892e540434e6ca48baa177670bb70ab89f
Benjamin Badger <blbadger10@gmail.com>
2024-10-08 19:11:47 -0400

    debugged

--- dataset/gen_run.sh
+++ dataset/gen_run.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python section_qa.py --n_gpus 2 --gpu_i 0 --model_path '/home/bbadger/Desktop/llama-3.1-8b-instruct_Q8_0.gguf' &
-CUDA_VISIBLE_DEVICES=1 python section_qa.py --n_gpus 2 --gpu_i 1 --model_path '/home/bbadger/Desktop/llama-3.1-8b-instruct-Q8_0.gguf'
+CUDA_VISIBLE_DEVICES=0 python section_qa.py --n_gpus 2 --gpu_i 0 --model_path '/home/bbadger/Desktop/llama-3-8b-instruct_Q8_0.gguf' &
+CUDA_VISIBLE_DEVICES=1 python section_qa.py --n_gpus 2 --gpu_i 1 --model_path '/home/bbadger/Desktop/llama-3-8b-instruct-Q8_0.gguf'
