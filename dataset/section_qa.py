37b3767c69bcc38d997a14500bc4989e1c111d93
Benjamin Badger <blbadger10@gmail.com>
2024-10-09 10:53:46 -0400

    debugged

--- dataset/section_qa.py
+++ dataset/section_qa.py
@@ -47,7 +47,7 @@ class QASections:
 		while start < len(text):
 			end = start + n_char
 			# continue until newline is found
-			while text[end] != '/n':
+			while text[end] != '/n' and end < len(text):
 				end += 1
 			chunks.append(text[start:end])
 			start = end + 1 # ignore newline for next text extract
