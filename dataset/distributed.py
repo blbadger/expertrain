6ea64e8587a8ed9f66c4fd0e2affdde44653a96b
Benjamin Badger <blbadger10@gmail.com>
2024-10-10 16:25:58 -0400

    cleaned

--- dataset/distributed.py
+++ dataset/distributed.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 	output_path = args.output_path
 	bash_string = ""
 	for gpu_index in range(n_gpus):
-		bash_string += template.format(gpu_index, n_gpus, gpu_index, model_path, output_path + f'_{gpu_index}')
+		bash_string += template.format(gpu_index, n_gpus, gpu_index, model_path, output_path)
 	bash_string = bash_string[:-3] # strip _&/n from last templated entry
 	print (f'Running string: {bash_string}')
 	subprocess.run(bash_string, shell=True)
