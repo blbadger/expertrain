4736fa7ad147785d2c015c1f996769d4d86d0171
blbadger <blbadger10@gmail.com>
2024-12-25 04:45:01 +0000

    updates

--- fastrain/run.sh
+++ fastrain/run.sh
@@ -1,11 +1,11 @@
 OMP_NUM_THREADS=10 accelerate launch --config_file "configs/fsdp_config_qlora.yaml" train.py \
 --seed 100 \
---model_name_or_path "/home/bbadger/Desktop/llama-3-8b-instruct" \
---dataset_path "/home/bbadger/experiments/github_pages_source" \
+--model_name_or_path "/home/bbadger/experiments/github_llama3_8b_merged" \
+--dataset_path "/home/bbadger/Desktop/github-qas-llama3" \
 --add_special_tokens False \
 --append_concat_token False \
 --max_seq_len 1024 \
---num_train_epochs 15 \
+--num_train_epochs 4 \
 --logging_steps 50 \
 --log_level "info" \
 --logging_strategy "steps" \
@@ -13,13 +13,13 @@ OMP_NUM_THREADS=10 accelerate launch --config_file "configs/fsdp_config_qlora.ya
 --save_strategy "epoch" \
 --bf16 False \
 --fp16 True \
---packing False \
+--packing True \
 --learning_rate 7e-5 \
 --lr_scheduler_type "linear" \
 --weight_decay 0.0 \
 --warmup_ratio 0.0 \
 --max_grad_norm 1.0 \
---output_dir "/home/bbadger/experiments/github_llama3.1_8b_qlora" \
+--output_dir "/home/bbadger/experiments/github_llama3.1_8b_qas_qlora" \
 --per_device_train_batch_size 4 \
 --per_device_eval_batch_size 4 \
 --gradient_checkpointing True \
