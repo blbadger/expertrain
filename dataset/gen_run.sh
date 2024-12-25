a3769bd40beebb5f7d160daa09d3435854dc24d7
blbadger <blbadger10@gmail.com>
2024-12-25 20:54:32 +0000

    updated

--- dataset/gen_run.sh
+++ dataset/gen_run.sh
@@ -1,1 +1,1 @@
-python distributed.py --n_gpus=4 --model_path='/home/bbadger/Desktop/llama-3.1-8b-instruct-Q8_0.gguf' --output_path '/home/bbadger/Desktop/github-qas-llama3.1'
+python distributed.py --n_gpus=4 --model_path='/home/bbadger/Desktop/llama-3-8b-instruct-Q8_0.gguf' --output_path '/home/bbadger/Desktop/github-qas-llama3'
