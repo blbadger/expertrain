e7413fca6cca0f0c1c3ef2bc32f7d5ab47e9faa2
blbadger <blbadger10@gmail.com>
2025-04-03 11:09:25 -0400

    unsloth test

--- fastrain/unsloth_run.sh
+++ fastrain/unsloth_run.sh
@@ -1,10 +1,10 @@
-OMP_NUM_THREADS=8 accelerate launch --config_file "configs/fsdp_config_qlora.yaml" train.py \
+OMP_NUM_THREADS=4 python train.py \
 --seed 100 \
---model_name_or_path "/home/bbadger/Desktop/llama-3.2-3b-instruct" \
+--model_name_or_path "/home/bbadger/Desktop/llama-3.1-8b-instruct" \
 --dataset_path "/home/bbadger/experiments/bird_train_dataset_completion" \
 --add_special_tokens False \
 --append_concat_token False \
---max_seq_length 4096 \
+--max_seq_length 8192 \
 --num_train_epochs 0 \
 --logging_steps 20 \
 --log_level "info" \
@@ -21,22 +21,17 @@ OMP_NUM_THREADS=8 accelerate launch --config_file "configs/fsdp_config_qlora.yam
 --weight_decay 0.0 \
 --warmup_ratio 0.0 \
 --max_grad_norm 1.0 \
---output_dir "/home/bbadger/experiments/llama3.2-3b-bird-lora" \
+--output_dir "/home/bbadger/experiments/llama3.1-8b-bird-unsloth" \
 --per_device_train_batch_size 1 \
 --per_device_eval_batch_size 1 \
 --gradient_checkpointing True \
 --use_reentrant True \
---gradient_accumulation_steps 4 \
+--gradient_accumulation_steps 8 \
 --dataset_text_field "messages" \
 --use_flash_attn False \
 --use_peft_lora True \
 --lora_r 64 \
 --lora_alpha 64 \
 --lora_dropout 0. \
---lora_target_modules "all-linear" \
---use_4bit_quantization True \
---use_nested_quant True \
---bnb_4bit_compute_dtype "float16" \
---bnb_4bit_quant_storage_dtype "float16" \
 --report_to "none" \
 # --resume_from_checkpoint "/home/bbadger/experiments/llama3.2-3b-bird-lora/checkpoint-2000"
--- fastrain/utils.py
+++ fastrain/utils.py
@@ -5,6 +5,35 @@ from datasets import DatasetDict, load_dataset, load_from_disk
 from datasets.builder import DatasetGenerationError
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import LoraConfig
+from unsloth import FastLanguageModel
+
+
+def prepare_unsloth(args, data_args, training_args, device=None):
+	model, tokenizer = FastModel.from_pretrained(
+	    model_name = args.model_name_or_path,
+	    max_seq_length = training_args.max_seq_length, # Choose any for long context!
+	    load_in_4bit = True,  # 4 bit quantization to reduce memory
+	    load_in_8bit = False, # [NEW!] A bit more accurate, uses 2x memory
+	    full_finetuning = False, # [NEW!] We have full finetuning now!
+	)
+	# Do model patching and add fast LoRA weights
+	model = FastLanguageModel.get_peft_model(
+	    model,
+	    r = args.lora_r,
+	    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+	                      "gate_proj", "up_proj", "down_proj",],
+	    lora_alpha = args.lora_alpha,
+	    lora_dropout = 0, # Supports any, but = 0 is optimized
+	    bias = "none",    # Supports any, but = "none" is optimized
+	    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+	    random_state = 3407,
+	    max_seq_length = training_args.max_seq_length,
+	    use_rslora = False,  # We support rank stabilized LoRA
+	    loftq_config = None, # And LoftQ
+	)
+	peft_config = []
+	return model, peft_config, tokenizer
+
 
 def create_and_prepare_model(args, data_args, training_args, device=None):
 	bnb_config = None
@@ -73,3 +102,54 @@ def create_and_prepare_model(args, data_args, training_args, device=None):
 		tokenizer.pad_token = tokenizer.eos_token
 
 	return model, peft_config, tokenizer
+
+
+def tile_inputs(input_ids, tokenizer, tile_overlap=20, tile_size=1024):
+	text_length = len(input_ids[0])
+	assert text_length >= tile_overlap, "Text must be longer than overlap to tile"
+
+	i, tiled_arr = 0, []
+	while i < text_length:
+		if i + tile_size <= text_length:
+			tokens = input_ids[0][i:i+tile_size]
+			tiled_arr.append(tokens)
+		else:
+			# pad the last tile
+			tokens = input_ids[0][i:i+tile_size]
+			pad_length = tile_size - len(tokens)
+			tokens = torch.nn.functional.pad(
+				tokens,
+				(0, pad_length),
+				mode='constant', 
+				value=tokenizer.pad_token_id
+				)
+			tiled_arr.append(tokens)
+		i += tile_size - tile_overlap
+	return tiled_arr
+
+def tokenize_input(text, tokenizer, tile_size=1024, overlap_size=20):
+	# assumes dataset is not large (< 10b samples) and can be loaded in memory
+	all_data = []
+	for i, text_file in enumerate(text):
+		if isinstance(text_file, dict):
+			text_file = text_file['text']
+		input_ids = tokenizer.encode(
+			text_file,
+			add_special_tokens=False,
+			return_tensors="pt",
+			truncation=False
+			)
+
+		if len(input_ids[0]) < overlap_size:
+			continue
+		data = tile_inputs(input_ids, tokenizer, tile_size=tile_size, tile_overlap=overlap_size)
+		all_data += data
+	return all_data
+
+
+def detokenize_input(tokens, tokenizer):
+	text = []
+	for i, tensor in enumerate(tokens):
+		text_input = tokenizer.decode(tensor, skip_special_tokens=True)
+		text.append(text_input)
+	return text
